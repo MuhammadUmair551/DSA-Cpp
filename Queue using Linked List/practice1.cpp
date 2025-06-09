411d3f3809c61019ab0fe67bce0595055aa2d364
Muhammad Umair <ua5600103@gmail.com>
2025-06-09 19:05:01 +0500

    deque

--- Queue using Linked List/practice1.cpp
+++ Queue using Linked List/practice1.cpp
@@ -95,21 +95,21 @@
 // Deque
 //its a double ended queue like adding from front and rear both and popping and seeing 
 
-#include<iostream>
-#include<deque>  //* 
-using namespace std;
-
-int main(){
-	deque<int> dq;  // *
-		
-	dq.push_back(1);
-	dq.push_back(2);
-	dq.push_back(3);
-	
-	dq.push_front(4);
-	
-	dq.pop_back();
-	cout<< dq.front() << " " << dq.back() << endl;
-	
-	return 0;
-}
+//#include<iostream>
+//#include<deque>  //* 
+//using namespace std;
+//
+//int main(){
+//	deque<int> dq;  // *
+//		
+//	dq.push_back(1);
+//	dq.push_back(2);
+//	dq.push_back(3);
+//	
+//	dq.push_front(4);
+//	
+//	dq.pop_back();
+//	cout<< dq.front() << " " << dq.back() << endl;
+//	
+//	return 0;
+//}
